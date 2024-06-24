8c565b6facf29f343ab34dc9008e24edcff7b5cb
Sober7135 <soberwu@outlook.com>
2024-06-25 01:19:45 +0800

    fix: user library println! and print!

--- user/src/console.rs
+++ user/src/console.rs
@@ -18,14 +18,17 @@ pub fn print(args: fmt::Arguments) {
 
 #[macro_export]
 macro_rules! print {
-    ($($($arg: tt)+)*) => {
-        $crate::console::print(format_args!($($($arg)+)?))
+    ($($arg: tt)*) => {
+        $crate::console::print(format_args!($($arg)?))
     };
 }
 
 #[macro_export]
 #[allow_internal_unstable(format_args_nl)]
 macro_rules! println {
+    () => {
+      $crate::print!("\n")
+    };
     ($($arg: tt)*) => {
         $crate::console::print(format_args_nl!($($arg)*))
     };
